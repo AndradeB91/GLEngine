d6228aa0599ffbc6fcd61c6793ea4497ab49faca
Lucas Andrade Benevides <lucas.andradebenevides@gmail.com>
2021-06-19 15:58:21 -0300

    Refacts main.cpp to now use Scene class

--- src/main.cpp
+++ src/main.cpp
@@ -15,6 +15,46 @@
 #include "Shader.h"
 #include "Camera.h"
 #include "Texture.h"
+#include "Light.h"
+#include "Scene.h"
+
+void calcAverageNormals(unsigned int *indices, unsigned int indiceCount, GLfloat *vertices, unsigned int verticeCount,
+                        unsigned int vLength, unsigned int normalOffset)
+{
+    for (size_t i = 0; i < indiceCount; i += 3)
+    {
+        unsigned int in0 = indices[i] * vLength;
+        unsigned int in1 = indices[i + 1] * vLength;
+        unsigned int in2 = indices[i + 2] * vLength;
+        glm::vec3 v1(vertices[in1] - vertices[in0], vertices[in1 + 1] - vertices[in0 + 1], vertices[in1 + 2] - vertices[in0 + 2]);
+        glm::vec3 v2(vertices[in2] - vertices[in0], vertices[in2 + 1] - vertices[in0 + 1], vertices[in2 + 2] - vertices[in0 + 2]);
+        glm::vec3 normal = glm::cross(v1, v2);
+        normal = glm::normalize(normal);
+
+        in0 += normalOffset;
+        in1 += normalOffset;
+        in2 += normalOffset;
+        vertices[in0] += normal.x;
+        vertices[in0 + 1] += normal.y;
+        vertices[in0 + 2] += normal.z;
+        vertices[in1] += normal.x;
+        vertices[in1 + 1] += normal.y;
+        vertices[in1 + 2] += normal.z;
+        vertices[in2] += normal.x;
+        vertices[in2 + 1] += normal.y;
+        vertices[in2 + 2] += normal.z;
+    }
+
+    for (size_t i = 0; i < verticeCount / vLength; i++)
+    {
+        unsigned int nOffset = i * vLength + normalOffset;
+        glm::vec3 vec(vertices[nOffset], vertices[nOffset + 1], vertices[nOffset + 2]);
+        vec = glm::normalize(vec);
+        vertices[nOffset] = vec.x;
+        vertices[nOffset + 1] = vec.y;
+        vertices[nOffset + 2] = vec.z;
+    }
+}
 
 Model *createPyramid()
 {
@@ -28,32 +68,44 @@ Model *createPyramid()
         2, 3, 0,
         0, 1, 2};
 
+    glm::mat4 modelMatrix(1.0f);
+    modelMatrix = glm::translate(modelMatrix, glm::vec3(0.0f, 0.0f, -2.5f));
+    modelMatrix = glm::scale(modelMatrix, glm::vec3(0.4f, 0.4f, 1.0f));
+
     GLfloat vertices[] = {
-        -1.0f, -1.0f, 0.0f, 0.0f, 0.0f,
-        0.0f, -1.0f, 1.0f, 0.5f, 0.0f,
-        1.0f, -1.0f, 0.0f, 1.0f, 0.0f,
-        0.0f, 1.0f, 0.0f, 0.5f, 1.0f};
+        -1.0f, -1.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f,
+        0.0f, -1.0f, 1.0f, 0.5f, 0.0f, 0.0f, 0.0f, 0.0f,
+        1.0f, -1.0f, 0.0f, 1.0f, 0.0f, 0.0f, 0.0f, 0.0f,
+        0.0f, 1.0f, 0.0f, 0.5f, 1.0f, 0.0f, 0.0f, 0.0f};
 
-    return new Model(vertexShader,
-                     fragmentShader,
-                     texturePath,
-                     vertices,
-                     indices,
-                     20,
-                     12);
+    calcAverageNormals(indices, 12, vertices, 32, 8, 5);
 
-    // GLfloat vertices[] = {
-    //     -1.0f, -1.0f, 0.0f,
-    //     0.0f, -1.0f, 1.0f,
-    //     1.0f, -1.0f, 0.0f,
-    //     0.0f, 1.0f, 0.0f};
+    Model *model = new Model(vertexShader,
+                             fragmentShader,
+                             texturePath,
+                             vertices,
+                             indices,
+                             32,
+                             12);
 
-    // return new Model(vertexShader,
-    //                  fragmentShader,
-    //                  vertices,
-    //                  indices,
-    //                  12,
-    //                  12);
+    model->setModelMatrix(modelMatrix);
+    return model;
+
+    //     GLfloat vertices[] = {
+    //         -1.0f, -1.0f, 0.0f,
+    //         0.0f, -1.0f, 1.0f,
+    //         1.0f, -1.0f, 0.0f,
+    //         0.0f, 1.0f, 0.0f};
+
+    //     Model *model = new Model(vertexShader,
+    //                              fragmentShader,
+    //                              vertices,
+    //                              indices,
+    //                              12,
+    //                              12);
+
+    //     model->setModelMatrix(modelMatrix);
+    //     return model;
 }
 
 int main()
@@ -64,15 +116,18 @@ int main()
     mainWindow.initialize();
 
     Camera camera = Camera();
-
-    std::vector<Model *> modelList;
-    std::vector<Model *>::iterator itModel;
-
-    modelList.push_back(createPyramid());
+    Light light = Light(1.0f, 1.0f, 1.0f, 0.2f,
+                        2.0f, -1.0f, -2.0f, 1.0f);
 
     glm::mat4 projection = glm::perspective(glm::radians(45.0f),
-                                            (GLfloat)mainWindow.getBufferWidth() / mainWindow.getBufferHeight(),
+                                            (GLfloat)mainWindow.getBufferWidth() /
+                                                mainWindow.getBufferHeight(),
                                             0.1f, 100.0f);
+
+    Scene scene = Scene();
+    scene.addModel(createPyramid());
+    scene.setProjectionMatrix(projection);
+    scene.setLight(light);
 
     while (!mainWindow.getShouldClose())
     {
@@ -86,28 +141,13 @@ int main()
         camera.listenKeys(mainWindow.getKeys(), deltaTime);
         camera.listenMouse(mainWindow.getXDelta(), mainWindow.getYDelta());
 
+        scene.setViewMatrix(camera.calculateViewMatrix());
+
         // Clear window
         glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-        for (itModel = modelList.begin(); itModel != modelList.end(); ++itModel)
-        {
-            (*itModel)->prepareToRender();
-
-            glm::mat4 model(1.0f);
-            model = glm::translate(model, glm::vec3(0.0f, 0.0f, -2.5f));
-            model = glm::scale(model, glm::vec3(0.4f, 0.4f, 1.0f));
-
-            glm::mat4 view = camera.calculateViewMatrix();
-
-            (*itModel)->setUniformMatrix4fv("model", model);
-            (*itModel)->setUniformMatrix4fv("view", view);
-            (*itModel)->setUniformMatrix4fv("projection", projection);
-
-            (*itModel)->setUniform3f("colour", glm::vec3(1.0f, 0.0f, 0.0f));
-
-            (*itModel)->render();
-        }
+        scene.renderModels();
 
         glUseProgram(0);
 
