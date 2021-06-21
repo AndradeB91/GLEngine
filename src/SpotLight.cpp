aff9416c81f34f66d79b5051f04f72ca58ba6adf
Lucas Andrade Benevides <lucas.andradebenevides@gmail.com>
2021-06-21 01:14:41 -0300

    Creates a SpotLight class

---
+++ src/SpotLight.cpp
@@ -0,0 +1,59 @@
+#include "SpotLight.h"
+
+SpotLight::SpotLight()
+{
+    this->_direction = glm::vec3(0.0f, -1.0f, 0.0f);
+    this->_edge = 0.0f;
+    this->_procEdge = cosf(glm::radians(this->_edge));
+}
+
+SpotLight::SpotLight(GLfloat red,
+                     GLfloat green,
+                     GLfloat blue,
+                     GLfloat ambientIntensity,
+                     GLfloat diffuseIntensity,
+                     GLfloat xPos,
+                     GLfloat yPos,
+                     GLfloat zPos,
+                     GLfloat xDir,
+                     GLfloat yDir,
+                     GLfloat zDir,
+                     GLfloat constant,
+                     GLfloat linear,
+                     GLfloat exponent,
+                     GLfloat edge) : PointLight(red,
+                                                green,
+                                                blue,
+                                                ambientIntensity,
+                                                diffuseIntensity,
+                                                xPos,
+                                                yPos,
+                                                zPos,
+                                                constant,
+                                                linear,
+                                                exponent)
+{
+    this->_direction = glm::normalize(glm::vec3(xDir, yDir, zDir));
+    this->_edge = edge;
+    this->_procEdge = cosf(glm::radians(this->_edge));
+}
+
+void SpotLight::setFlash(glm::vec3 position, glm::vec3 direction)
+{
+    this->_position = position;
+    this->_direction = direction;
+}
+
+glm::vec3 SpotLight::getDirection()
+{
+    return this->_direction;
+}
+
+GLfloat SpotLight::getProcEdge()
+{
+    return this->_procEdge;
+}
+
+SpotLight::~SpotLight()
+{
+}
---
+++ src/SpotLight.h
@@ -0,0 +1,39 @@
+#pragma once
+
+#include "PointLight.h"
+
+class SpotLight : public PointLight
+{
+
+public:
+    SpotLight();
+
+    SpotLight(GLfloat red,
+              GLfloat green,
+              GLfloat blue,
+              GLfloat ambientIntensity,
+              GLfloat diffuseIntensity,
+              GLfloat xPos,
+              GLfloat yPos,
+              GLfloat zPos,
+              GLfloat xDir,
+              GLfloat yDir,
+              GLfloat zDir,
+              GLfloat constant,
+              GLfloat linear,
+              GLfloat exponent,
+              GLfloat edge);
+
+    glm::vec3 getDirection();
+
+    void setFlash(glm::vec3 position, glm::vec3 direction);
+
+    GLfloat getProcEdge();
+
+    ~SpotLight();
+
+private:
+    glm::vec3 _direction;
+
+    GLfloat _edge, _procEdge;
+};
